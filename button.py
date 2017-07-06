842804bbc9f5253e506023e94d87eedd6ae5cbe7
Jason Arends <jason.arends@gmail.com>
2017-07-06 15:45:04 -0500

    tweaked colors

--- button.py
+++ button.py
@@ -11,7 +11,7 @@ from picamera import PiCamera
 
 def signalHandler(signal, frame):
 	#signalHandler catches ctrl-C and cleanly exits
-	GPIO.output(greenLED, False)
+	GPIO.output(blueLED, False)
 	GPIO.cleanup()
 	syslog.syslog('Caught SIGTERM, exiting.')
 	sys.exit(0)
@@ -42,15 +42,15 @@ def pickleWriter(f, data):
 
 def handle(pin):
 	#handles a button press on a gpio pin, and flips logData to start/stop logging
-	global logData, whiteLED, redLED, greenLED
+	global logData, greenLED, redLED, blueLED
 
 	syslog.syslog('Button pressed.')
 
 	logData = not pickleReader('button.pickle') #since each button press flips it, we want opposite value of what's stored
 
 	syslog.syslog('logData changed to %s' %logData)
-
-	GPIO.output(whiteLED, logData)
+	GPIO.output(blueLED, False) #no blue, just green or red after button
+	GPIO.output(greenLED, logData)
 	GPIO.output(redLED, not logData)
 
 	pickleWriter('button.pickle',logData)
@@ -64,22 +64,22 @@ def handle(pin):
 
 def gpioSetup():
 	#init for GPIO
-	global buttonPin, redLED, whiteLED, greenLED
+	global buttonPin, redLED, greenLED, blueLED
 
 	#use broadcom numbers, not board pins
 	GPIO.setmode(GPIO.BCM)
 
 	buttonPin = 23
 	redLED = 17
-	whiteLED = 27
-	greenLED = 22
+	greenLED = 27
+	blueLED = 22
 
 	GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # buttonPin is an input, with a pullup resistor to set it to high by default. pressing button changes it to low
 	GPIO.setup(redLED, GPIO.OUT)
-	GPIO.setup(whiteLED, GPIO.OUT)
 	GPIO.setup(greenLED, GPIO.OUT)
+	GPIO.setup(blueLED, GPIO.OUT)
 
-	GPIO.output(greenLED, True)
+	GPIO.output([greenLED,redLED,blueLED], True) #white on init
 	GPIO.add_event_detect(23, GPIO.FALLING, handle, bouncetime=250)
 
 def initPressure():
@@ -118,25 +118,29 @@ def getBCM280():
 
 def snapPhoto():
 	#syslog.syslog('snapPhoto')
-	global uniqueID
+	global uniqueID, redLED
 	with PiCamera() as camera:
 		camera.resolution = (3280, 2464)
 		time.sleep(2)
+		GPIO.output(redLED, True)
 		timestr = time.strftime("%Y%m%d-%H%M%S")
 		filename = '/home/pi/balloonlogger/photo/' + uniqueID + '_img_' + timestr + '.jpg'
 		camera.capture(filename)
+		GPIO.output(redLED, False)
 
 def takeVid():
 	#syslog.syslog('takeVid')
-	global uniqueID
+	global uniqueID, redLED
 	with PiCamera() as camera:
 		camera.resolution = (1640, 922)
 		time.sleep(2)
+		GPIO.output(redLED, True)
 		timestr = time.strftime("%Y%m%d-%H%M%S")
 		filename = '/home/pi/balloonlogger/video/' + uniqueID + '_vid_' + timestr + '.h264'
 		camera.start_recording(filename)
 		camera.wait_recording(50)
 		camera.stop_recording()
+		GPIO.output(redLED, False)
 
 def calcAlt(zeroPress, currPress):
 	#syslog.syslog('calcAlt zeroPress: %s' % zeroPress)
@@ -153,29 +157,34 @@ def xmitData(temp1,temp2,pres1,alt):
 		time.sleep(0.1)
 
 def logLoop():
-	global logData
-	global zPress
-	global uniqueID
+	global logData, zPress, uniqueID
+	global redLED, greenLED, blueLED
 	if logData:
 		logFile = open('/home/pi/balloonlogger/data/' + uniqueID + '_data.csv','a')
 		csvWriter = csv.writer(logFile)
 		zPress = initPressure()
 	while logData:
+		GPIO.output([redLED,blueLED], False)
 		timestamp = math.trunc(time.time())
 		syslog.syslog('start log loop %s' % timestamp)
-		GPIO.output(redLED, True)
+		GPIO.output(blueLED, True)
+		GPIO.output(greenLED, False)
 		t1 = getDS18b20()
 		p1, t2 = getBCM280()
-		GPIO.output(redLED, False)
+		GPIO.output(greenLED, True)
+		GPIO.output(blueLED, False)
 		alt = calcAlt(zPress, p1)
 		csvWriter.writerow([timestamp,t1,t2,p1,alt])
 		logFile.flush()
 		os.fsync(logFile.fileno())
-		GPIO.output(redLED, True)
+		GPIO.output([blueLED,redLED], True)
+		GPIO.output(greenLED, False)
 		xmitData(t1,t2,p1,alt)
-		GPIO.output(redLED, False)
+		GPIO.output([redLED,blueLED],False)
+		GPIO.output(greenLED, True)
 		snapPhoto()
 		takeVid()
+		GPIO.output([blueLED, redLED], False)
 		loopDuration = math.trunc(time.time()) - timestamp
 		if loopDuration < 60:
 			t = 60 - loopDuration
